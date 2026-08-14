98ad03528184130ed0a720cf72b7701098c7737a
Yash Parmar <142144435+YashParmar27@users.noreply.github.com>
2026-08-14 23:31:19 +0530

    Add: score-of-a-string - submission-0

---
+++ Data Structures & Algorithms/score-of-a-string/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int score = 0;
+        for (int i = 1; i < s.length(); i++)
+        {
+            int s1 = s.charAt(i);
+            int s2 = s.charAt(i - 1);
+
+            score += Math.abs(s1 - s2);
+        }
+        return score;
+    }
+}
